e4498bde69508668cdb0330a8077515126db806d
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-12 08:34:17 +0100

    Commit

--- app.rb
+++ app.rb
@@ -1,14 +1,27 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
-get  '/' do
-  'Hello world'
+get '/' do
+  'Hello!'
 end
 
 get '/secret' do
-  'This is a secret'
+  'This is a secret, ok'
 end
 
-get '/mytestroute' do
-    send_file 'test.html'
- end
+get '/public' do
+  'AW17'
+end
+
+get '/cat' do
+  send_file 'cat.html'
+end
+
+get '/cat-erb' do
+  erb(:index)
+end
+
+
+get '/student' do
+  erb(:student)
+end
